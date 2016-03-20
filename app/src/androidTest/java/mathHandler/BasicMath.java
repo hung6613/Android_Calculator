b89cf48cf829ce2080f6cc6f82162fba5476ed92
hung6613 <cauchunho1413@gmail.com>
2016-03-20 20:06:46 +0700

    update math handle

--- app/src/androidTest/java/mathHandler/BasicMath.java
+++ app/src/androidTest/java/mathHandler/BasicMath.java
@@ -5,5 +5,34 @@ package mathHandler;
  */
 public class BasicMath {
 
+    public static double plus(double a, double b){
+        return a+b;
+    }
+    public static double multiply(double a, double b){
+        return a*b;
+    }
+    public static double divide(double a, double b){
+        if(b==0)    return Double.parseDouble(null);
+        else return a/b;
+    }
+    public static double minus(double a, double b){
+        return a-b;
+    }
+    public static double reverse(double a){
+        return -1*a;
+    }
+    public static double xMu2(double x){
+        return x*x;
+    }
+    public static double motPhanX(double x){
+        if(x==0)    return Double.parseDouble(null);
+        else    return 1/x;
+    }
+    public static double sqrtX(double x){
+        if(x < 0)   return Double.parseDouble(null);
+        else return Math.sqrt(x);
+    }
+
+
 
 }
